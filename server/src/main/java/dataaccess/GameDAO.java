c8d9417022e59c38cd9fa23a77577c7c7ed19302
Princedg5678 <gilesdevyn6@gmail.com>
2025-01-16 12:57:25 -0700

    Started on SQLGameDAO

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -6,10 +6,10 @@ import model.GameName;
 import java.util.Map;
 
 public interface GameDAO {
-    public Integer createGame(GameName gameName);
-    public Map<Integer, GameData> listGames();
-    public GameData findGame(Integer gameID);
-    public void addPlayer(GameData gameData, String playerColor, String username);
-    public void clearGames();
+    public Integer createGame(GameName gameName) throws DataAccessException;
+    public Map<Integer, GameData> listGames() throws DataAccessException;
+    public GameData findGame(Integer gameID) throws DataAccessException;
+    public void addPlayer(GameData gameData, String playerColor, String username) throws DataAccessException;
+    public void clearGames() throws DataAccessException;
 
 }
--- server/src/main/java/dataaccess/SQLGameDAO.java
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -1,33 +1,84 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.GameData;
+import model.GameID;
 import model.GameName;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.HashMap;
 import java.util.Map;
 
-public class SQLGameDAO implements GameDAO{
+public class SQLGameDAO implements GameDAO {
     @Override
-    public Integer createGame(GameName gameName) {
-        return 0;
-    }
+    public Integer createGame(GameName gameName) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()){
+            try (PreparedStatement preparedStatement =
+                         conn.prepareStatement("INSERT INTO games (whiteUsername, blackUsername, " +
+                                 "gameName, game) VALUES (?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, null);
+                preparedStatement.setString(2, null);
+                preparedStatement.setString(3, new Gson().toJson(gameName));
+                preparedStatement.setString(4, new Gson().toJson(new ChessGame()));
+                preparedStatement.executeUpdate();
+                var returnKeys = preparedStatement.getGeneratedKeys();
+                if (returnKeys.next()){
+                    return returnKeys.getInt(1);
+                }
+            }
 
-    @Override
-    public Map<Integer, GameData> listGames() {
-        return Map.of();
-    }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
 
-    @Override
-    public GameData findGame(Integer gameID) {
         return null;
     }
 
     @Override
-    public void addPlayer(GameData gameData, String playerColor, String username) {
+    public Map<Integer, GameData> listGames() throws DataAccessException {
+        Map<Integer, GameData> gameMap = new HashMap<>();
+        try (var conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM games")) {
+                try (var result = preparedStatement.executeQuery()){
+                    while (result.next()){
+                        int gameID = result.getInt(1);
+                        String whiteUsername = result.getString(2);
+                        String blackUsername = result.getString(3);
+                        String gameName = result.getString(4);
+                        ChessGame game = new Gson().fromJson(result.getString(5), ChessGame.class);
+                        GameData currentGame = new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+                        gameMap.put(gameID, currentGame);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+
+        return gameMap;
+    }
+
+    @Override
+    public GameData findGame(Integer gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void addPlayer(GameData gameData, String playerColor, String username) throws DataAccessException {
 
     }
 
     @Override
-    public void clearGames() {
-
+    public void clearGames() throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()){
+            try (PreparedStatement preparedStatement = conn.prepareStatement("TRUNCATE TABLE games")) {
+                preparedStatement.executeUpdate();
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 }
+
